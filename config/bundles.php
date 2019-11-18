31cfa63b00ab9df035de79660936e66dc33ae9bb
Emilien-Gts <emilien.gantois.pro@gmail.com>
2019-11-18 21:44:54 +0100

    remove all files before new install

--- config/bundles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-];
--- server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-php bin/console server:run
-pause
