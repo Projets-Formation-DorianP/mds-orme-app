e39244e62bac7a2eef41e5b23e64c6c20c5eef9c
Emilien-Gts <emilien.gantois.pro@gmail.com>
2020-02-03 21:13:52 +0100

    Add logout

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -10,8 +10,18 @@ class AccountController extends AbstractController
     /**
      * @Route("/login", name="account_login")
      */
-    public function index()
-    {
+    public function index() {
         return $this->render('account/login.html.twig');
+    }
+
+    /**
+     * Permet de se déconnecter
+     * 
+     * @Route("/logout", name="account_logout")
+     *
+     * @return void
+     */
+    public function logout() {
+        //Silence is golden...
     }
 }
